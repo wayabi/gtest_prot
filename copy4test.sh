6cf5bb5a8abb128e3e49ff0d27d87d2822a40bee
wayabi <wayabi.software@gmail.com>
2018-02-07 16:51:11 +0900

    add copy4test

---
+++ copy4test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+for h in `ls src/*.h`
+do
+	name=${h##*/}
+	cat src/${name} | sed -e "s/private:/public:/g" | sed -e "s/protected:/public:/g" > public_src/${name}
+done
+
+cp src/*.cpp public_src/
